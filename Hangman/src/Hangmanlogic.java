017d01fe69cd9963bfb4b23b29abd6eb9307db0a
marcus johansson <mj.emeth@gmail.com>
2020-10-07 20:58:22 +0200

    test

--- Hangman/src/Hangmanlogic.java
+++ Hangman/src/Hangmanlogic.java
@@ -2,44 +2,62 @@ import java.util.Scanner;
 
 public class Hangmanlogic {
 
-
-	private static String word = "Hyponeurikostiskadiafragmakontravibrationer";
-	private static String hideWord = "___________________________________________";
-	private static int count = 0;
+//	private static String word = "Hyponeurikostiskadiafragmakontravibrationer";
+//	private static String hideWord = "___________________________________________";
+// 	private static int count = 0;
 
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 
-		while (hideWord.contains("_")&&count < 7  ) {
-			System.out.println("Guess a letter");
-			System.out.println(hideWord);
-			String guess = reader.next();
-			hang(guess);
-		}
-	} 
+		HangmanRound aRound = new HangmanRound();
 
-	public static void hang(String guess) {
+		runGame(aRound, reader);
+
+		HangmanRound aRoundtwo = new HangmanRound();
+
+		runGame(aRoundtwo, reader);
+
+		reader.close();
+
+	}
+
+	private static void runGame(HangmanRound aRound, Scanner reader) {
+
+		StaticWordselection.setWord(aRound);
+
+		while (aRound.getShowWord().contains("_") && aRound.getScoreCount() < StaticWordselection.getMaxScore()) {
+			System.out.println("Guess a letter");
+			System.out.println(aRound.getShowWord());
+			String guess = reader.next();
+			hang(guess, aRound);
+		}
+		System.out.println("Game over");
+
+	}
+
+	public static void hang(String guess, HangmanRound currentRound) {
 		String testString = "";
-		for (int i = 0; i < word.length(); i++) {
-			if (word.charAt(i) == guess.charAt(0)) {
+		for (int i = 0; i < currentRound.getCurrentWord().length(); i++) {
+			if (currentRound.getCurrentWord().charAt(i) == guess.charAt(0)) {
+
 				testString += guess.charAt(0);
-			} else if (hideWord.charAt(i) != '_') {
-				testString += word.charAt(i);
+			} else if (currentRound.getShowWord().charAt(i) != '_') {
+
+				testString += currentRound.getCurrentWord().charAt(i);
 			} else {
 				testString += "_";
 			}
 		}
 
-		if (hideWord.equals(testString)) {
-			count++;
+		if (currentRound.getShowWord().equals(testString)) {
+			currentRound.addScoreCount();
 			System.out.println("wrong!");
 		} else {
-			hideWord = testString;
+			currentRound.setShowWord(testString);
 		}
-		if (hideWord.equals(word)) {
+		if (currentRound.getShowWord().equals(currentRound.getCurrentWord())) {
 			System.out.println("You win");
 		}
 	}
 
-	
 }
